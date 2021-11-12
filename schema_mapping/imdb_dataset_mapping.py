c6019d9128c3a22f8d5034fca0f41160318ce27a
ashishrana160796 <ashishrana160796@gmail.com>
2021-11-12 22:32:44 +0530

    mapping bug fixes for imdb dataset

--- schema_mapping/imdb_dataset_mapping.py
+++ schema_mapping/imdb_dataset_mapping.py
@@ -1,18 +1,26 @@
 import pandas as pd
 import numpy as np
-
 from constants import MOVIES_DATA_DIR
-from utils import get_integrated_schema_target_df
+from utils import get_integrated_schema_target_df, to_xml
 
 target_df = get_integrated_schema_target_df()
 
 imdb_df = pd.read_csv(MOVIES_DATA_DIR.joinpath('imdb.csv'))
 
-# Rename columns name and remove underscore(_) from columns
+# rename columns name and remove underscore(_) from columns
 imdb_df.columns = imdb_df.columns.str.replace('_', ' ')
 imdb_df = imdb_df.rename(columns={'imdb title id': 'title id',
                                   'actors': 'actor names',
-                                  'date published': 'release date'})
+                                  'year': 'release year',
+                                  'votes': 'imdb votes'}) # , 
+                                  # 'date published': 'release date'})
+# 'date published' field data needs to be transformed into 'release date' attribute.
+# Or we can directly map the 'year' attribute in the dataframe to integrated schema.
+
+# mapping 1-to-1 non-list attributes.
+lower_copy_columns = ['title id','title', 'country', 'release year','duration','avg vote', 'imdb votes']
+target_df[[col.lower() for col in lower_copy_columns]] = imdb_df[lower_copy_columns]
+target_df['imdb score'] = imdb_df['avg vote']
 
 # Attributes that have to be split into list[string]
 imdb_df_map = ({'genre': 'genre',
@@ -28,13 +36,9 @@ imdb_df[list(imdb_df_map.keys())] = imdb_df[imdb_df_map.values()].apply(
 target_df[list(imdb_df_map.keys())] = imdb_df[imdb_df_map.values()].apply(
     lambda column: column.str.split(', '))
 
-# Transform release date to datetime
-target_df['release date'] = pd.to_datetime(imdb_df['release date'], errors='coerce')
-
 # make all budget into dollar and delete other currency and change datatype of budget
 imdb_df['budget'] = imdb_df['budget'].astype(str).replace(r"[\a-zA-Z|]", np.nan)
-imdb_df['budget'] = imdb_df['budget'].str.extract('(\d+)', expand=False)
-target_df['budget'] = target_df['budget'].astype(float)
+target_df['budget'] = imdb_df['budget'].str.extract('(\d+)', expand=False)
 
 target_df['source'] = 'imdb'
 
@@ -55,3 +59,6 @@ target_df = target_df[cols]
 
 # replacing the spaces in columns to create xml compliant tags
 target_df.columns = target_df.columns.str.replace(' ', '_')
+
+# saving the 'target_df' dataset as integrated schema xml file
+target_df.to_xml(MOVIES_DATA_DIR.joinpath('imdb.xml'))
